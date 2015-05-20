179a525d6086080618f5d9ae06f036c65ec26bb8
Joseph Janiga <jjaniga@billtrust.com>
2015-05-20 15:06:51 -0400

    updates and reorgs

---
+++ dv-scrape.js
@@ -0,0 +1,51 @@
+var phantom = require('phantom');
+
+phantom.create(function(ph) {
+  return ph.createPage(function(page) {
+    return page.open("http://dv.njtransit.com/mobile/tid-mobile.aspx?sid=MP", function(status) {
+      
+      console.log("opened departure vision?", status);
+      page.includeJs('http://code.jquery.com/jquery-1.11.3.min.js', function(err) {
+        
+        setTimeout(function(){
+          return page.evaluate(function(){
+              
+              /**
+               * Evaluate this javascript from within the target page
+               */
+              var out = [];
+  
+              $('tr[onclick]').each(function(){
+
+                var tds = $(this).children('td');
+                
+                var json = {
+                  departs:  $(tds[0]).text().replace('\n ', '').replace(' \n', ''),
+                  to:       $(tds[1]).text(),
+                  trk:      $(tds[2]).text(),
+                  line:     $(tds[3]).text(),
+                  train:    $(tds[4]).text(),
+                  status:   $(tds[5]).text()
+                };
+            
+                out.push(json);
+                
+              });
+              
+              return out;
+
+          }, function(result){
+            
+            console.log(result);
+            ph.exit();
+            
+          });
+        }, 10);
+        
+      });
+      
+    });
+  });
+}, {
+  dnodeOpts: {weak: false}
+});
