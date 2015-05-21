2183976a3c5571dadba718f13fe15b3553543334
josephjaniga <josephjaniga@gmail.com>
2015-05-20 22:04:20 -0400

    updated the train details into an express server

--- dv-train-details.js
+++ dv-train-details.js
@@ -1,61 +1,80 @@
-var phantom = require('phantom');
-var train = process.argv[2] || "3821";
+var phantom = require('phantom'),
+    express = require('express'),
+    app = express();
 
-phantom.create(function(ph) {
-  return ph.createPage(function(page) {
-    return page.open("http://dv.njtransit.com/mobile/train_stops.aspx?train="+train, function(status) {
+app.get('/train/*', function(req, res){
+  
+  var path = req.originalUrl,
+      a = path.indexOf("/train/"),
+      e = path.substring(a+7),
+      train = e || "3821",
+      result = {};
       
-      page.includeJs('http://code.jquery.com/jquery-1.11.3.min.js', function(err) {
-        setTimeout(function(){
-          
-          return page.evaluate(function(){
+      train = train.replace("/", "");
+      
+  phantom.create(function(ph) {
+    return ph.createPage(function(page) {
+      return page.open("http://dv.njtransit.com/mobile/train_stops.aspx?train="+train, function(status) {
+        
+        page.includeJs('http://code.jquery.com/jquery-1.11.3.min.js', function(err) {
+          setTimeout(function(){
             
-              var towards = $('span#label_description b:nth-of-type(2)').text(),
-                  rows = $('table#table_stops tbody tr'),
-                  route = [];
-              $(rows).each(function(){
-                
-                if ( $(this).text() !== '' ){
+            return page.evaluate(function(){
+              
+                var towards = $('span#label_description b:nth-of-type(2)').text(),
+                    rows = $('table#table_stops tbody tr'),
+                    route = [];
+                $(rows).each(function(){
                   
-                  var d = $(this).find('i').text(),
-                      s = $(this).clone().find('i').remove().end().text().trim(),
-                      pos = s.indexOf('  at   ');
-                      
-                  // if theres a time "  at   "
-                  if ( pos !== -1 ){
-                    d = s.substring(pos+7);
-                    s = s.substring(0, pos);
-                  }
-                      
-                  var kvp = {
-                      	 	status: d,
-                          station: s.trim()
-                      	};
+                  if ( $(this).text() !== '' ){
+                    
+                    var d = $(this).find('i').text(),
+                        s = $(this).clone().find('i').remove().end().text().trim(),
+                        pos = s.indexOf('  at   ');
                         
-    	            route.push(kvp);
-                }
-              });
+                    // if theres a time "  at   "
+                    if ( pos !== -1 ){
+                      d = s.substring(pos+7);
+                      s = s.substring(0, pos);
+                    }
+                        
+                    var kvp = {
+                        	 	status: d,
+                            station: s.trim()
+                        	};
+                          
+      	            route.push(kvp);
+                  }
+                });
+                
+                var trainInfo = {
+                  toward: towards,
+                  route: route
+                };
+                
+                return trainInfo;
+                
+            }, function(results){
               
-              var trainInfo = {
-                toward: towards,
-                route: route
-              };
+              result = results;
+              result.train = train;
+              console.log("hit: " + train);
+              res.json(result);
+              ph.exit();
               
-              return trainInfo;
-              
-          }, function(result){
+            });
             
-            result.train = train;
-            console.log(result);
-            ph.exit();
-            
-          });
-          
-        }, 10);
+          }, 10);
+        });
       });
     });
-  });
-}, {
-  // necessary workaround for python depency issues?
-  dnodeOpts: {weak: false}
+  },  // end phantom
+  { dnodeOpts: {weak: false} }); // necessary workaround for python depency issues?
+  
+}); // end server route
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Train Information http://%s:%s', "localhost", port);
 });
